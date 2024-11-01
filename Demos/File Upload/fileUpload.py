9e800e8e6972731258dc23bc06ebd919cf2508b2
Gaskell-Matthew-1102 <112650395+Gaskell-Matthew-1102@users.noreply.github.com>
2024-11-01 16:33:51 -0700

    *Actual* python code for file upload and validation checking

---
+++ Demos/File Upload/fileUpload.py
@@ -0,0 +1,80 @@
+import os
+from tkinter.filedialog import askdirectory
+from os import walk
+
+#Opens file explorer, allowing user to select the folder containing their video files
+def get_video_path():
+    video_path = '{}'.format(askdirectory(title='Choose a directory', initialdir=r'C:\ '))
+    return video_path
+
+#Opens file explorer, allowing user to select the folder containing their data files
+def get_data_path():
+    data_path = '{}'.format(askdirectory(title='Choose a directory', initialdir=r'C:\ '))
+    return data_path
+
+#Function to count the number of files in a folder
+def count_files(directory_path):
+    count = 0
+    for filename in os.listdir(directory_path):
+        filepath = os.path.join(directory_path, filename)
+        if os.path.isfile(filepath):
+            count = count + 1
+    return count
+
+#Function to count the number of folders within a folder
+def count_folders(directory_path):
+    count = 0
+    for filename in os.listdir(directory_path):
+        filepath = os.path.join(directory_path, filename)
+        if os.path.isdir(filepath):
+            count = count + 1
+    return count
+
+#Function that returns a list of files within a folder
+def list_files(directory_path):
+    file_list = []
+    for filename in os.listdir(directory_path):
+        path = os.path.join(directory_path, filename)
+        if os.path.isfile(path):
+            file_list.append(filename)
+    return file_list
+
+#Function that returns the type of file
+def get_file_type(directory_path):
+    #change file_name to an _ if unused (made variable just incase)
+    file_name, file_type = os.path.splitext(directory_path)
+    return file_type
+
+#Function to validate that the user's selected video folder is correct and valid
+def validate_video_path(vpath):
+    file_count = count_files(vpath)
+    # print("Number of files: ", file_count)
+    if file_count != 4:
+        return False
+    file_list = list_files(vpath)
+    mp4_count = 0
+    csv_count = 0
+    for filename in file_list:
+        file_type = get_file_type(filename)
+        if file_type == '.mp4':
+            mp4_count = mp4_count + 1
+        elif file_type == '.csv':
+            csv_count = csv_count + 1
+    if mp4_count == 3 and csv_count == 1:
+        return True
+    else:
+        return False
+
+#Function to validate that the user's selected data folder is correct and valid
+def validate_data_path(dpath):
+    file_count = count_files(dpath)
+    folder_count = count_folders(dpath)
+    if folder_count > 1 and 20 >= file_count <= 10:
+        return False
+    required_files = ["eye0_timestamps.npy", "eye0.pldata", "eye1_timestamps.npy", "eye1.pldata",
+                      "accel_timestamps.npy", "accel.pldata", "gyro_timestamps.npy", "gyro.pldata"]
+    file_list = list_files(dpath)
+    for filename in file_list:
+        if filename not in required_files:
+            return False
+    return True
