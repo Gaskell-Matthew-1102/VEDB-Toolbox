78754e6fa6f3b65709d9060b0915219234033c8d
Tyler <tdishman@nevada.unr.edu>
2024-11-04 20:38:55 -0800

    Worked on user form validation

--- Login Screen Demo/login.py
+++ Login Screen Demo/login.py
@@ -1,6 +1,5 @@
-import flask
 import user
-from flask import redirect, Flask
+from flask import Flask, redirect, request
 from flask_login import LoginManager, logout_user
 
 login_manager = LoginManager()
@@ -16,30 +15,36 @@ app.config['SERVER_NAME'] = 'localhost:5000'
 login_manager.init_app(app)
 
 @login_manager.user_loader
-def load_user(user_email: str)
-    return user.get(user_email)
+def load_user(user_email: str):
+    return user.User.get(user_email)
 
+def validate_login(username: str, password: str) -> bool:
+    # This should hit the DB to check against username and password
+    # dummy data used here instead
+    
+    dummyUser = "waltuh"
+    dummyPass = "white"
+
+    return(username == dummyUser and password == dummyPass)
+
+#Rename to application name
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if validate_login(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
 
-    #These lines may need to be connected up to a database
-    form = LoginForm()
-    if form.validate_on_submit():
-        login_user()
-
-        flask.flash('Successfully logged in!')
-
-        next = flask.request.args.get('next')
-
-        #can maybe comment out, or write function for eventual actual hosted url
-        if not url_has_allowed_host_and_scheme(next, request.host):
-            return flask.abort(400)
-
-        return flask.redirect(next or flask.url_for('index'))
-    return flask.render_template('login.html', form=form)
-
-#Rename redirect
+#Rename to application name, rename redirect
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(somewhere)
+
+@app.route("/")
+def guh():
+    return "<p>hi<p>"
+
