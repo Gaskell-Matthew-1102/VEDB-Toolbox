4390b2959e5f8b35af5e62021c050cc903f95304
Brian Wu <brianwu@unr.edu>
2024-11-10 03:59:05 +0000

    fixed hopefully

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(256), unique=True, nullable=False)
-    email = db.Column(db.String(512), unique=True, nullable=False)
+    email = db.Column(db.String(512), nullable=False)
     password = db.Column(db.String(256), nullable=False, default="")
     administrator = db.Column(db.Boolean, default=False)
 
--- routes.py
+++ routes.py
@@ -4,41 +4,55 @@ from flask import render_template, request, redirect, flash, url_for
 from flask_login import login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, Users
+from forms import LoginForm, RegistrationForm
 
 # Register route
 def register():
-    if request.method == "POST":
-        if Users.query.filter_by(username=request.form.get("username")).first():
+    # create WTForm instance
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        # check if user already exists
+        if Users.query.filter_by(username=form.username.data).first():
             flash("Username already in use!", "danger")
+            return redirect(url_for("register"))   
+
+        # check if passwords match
+        if not form.password.data == form.repeat_password.data:
+            flash("Passwords not matching!", "danger")
             return redirect(url_for("register"))
 
-        if request.form.get("password") != request.form.get("repeat-password"):
-            flash("Passwords do not match!", "danger")
-            return redirect(url_for("register"))            
+        # creates user
+        user = Users(username=form.username.data, email=form.email.data, password=generate_password_hash(form.password.data))
 
-        password_hash = generate_password_hash(request.form.get("password"))
-        user = Users(username=request.form.get("username"), email=request.form.get("email"), password=password_hash)
-
+        # write to DB
         db.session.add(user)
         db.session.commit()
 
+        # login user after creation
         login_user(user)
+        flash("Registration successful! Welcome.", "success")
         return redirect(url_for("home"))
 
-    return render_template("register.html")
+    return render_template("register.html", form=form)
 
 # Login route
 def login():
-    if request.method == "POST":
-        user = Users.query.filter_by(username=request.form.get("username")).first()
+    # Create LoginForm (WTForm)
+    form = LoginForm()
 
-        if user and check_password_hash(user.password, request.form.get("password")):
+    if form.validate_on_submit():
+        # Query DB for first user with inputted username
+        user = Users.query.filter_by(username=form.username.data).first()
+
+        if user and check_password_hash(user.password, form.password.data):
             login_user(user)
+            flash("Login successful!", "success")
             return redirect(url_for("home"))
         else:
             flash("Invalid username/password!", "danger")
 
-    return render_template("login.html")
+    return render_template("login.html", form=form)
 
 # Logout route
 def logout():
