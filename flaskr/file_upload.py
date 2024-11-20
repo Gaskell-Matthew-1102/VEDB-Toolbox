3e675ab945feca95d96ede49cb4e5afd3459bd55
Gaskell-Matthew-1102 <112650395+Gaskell-Matthew-1102@users.noreply.github.com>
2024-11-20 12:37:56 -0800

    Progress on video link upload, still needs testing

--- flaskr/file_upload.py
+++ flaskr/file_upload.py
@@ -12,23 +12,26 @@ from io import BytesIO
 import zipfile
 import requests
 
-
-# app = app
-
+#Global variables
+#File lists
 video_file_list = []
 data_file_list = []
 
+#Forms for file/link upload
 show_form1 = True
 show_form2 = True
 
+#Failure variables (used in error message display within HTML)
 failed_video_upload = False
 failed_data_upload = False
 failed_video_link = False
 failed_data_link = False
 
+#Extra folder variables (might not need these??)
 are_videos_in_folder = False
 is_data_in_folder = False
 
+#Folder name variables (definitely need these)
 video_folder_name = ""
 data_folder_name = ""
 
@@ -47,6 +50,7 @@ def set_showform(form_num: int, flag: bool) -> None:
         global show_form2
         show_form2 = flag
 
+# Setter for if an extra folder exists
 def set_is_folder(form_num: int, flag: bool, name: str) -> None:
     if form_num == 1:
         global are_videos_in_folder
@@ -59,12 +63,14 @@ def set_is_folder(form_num: int, flag: bool, name: str) -> None:
         global data_folder_name
         data_folder_name = name
 
+# Getter for if an extra folder exists
 def get_is_folder(form_num: int) -> bool:
     if form_num == 1:
         return are_videos_in_folder
     elif form_num == 2:
         return is_data_in_folder
 
+# Failure setters
 def set_failed_link(form_num: int, failed: bool) -> None:
     if form_num == 1:
         global failed_video_link
@@ -81,6 +87,7 @@ def set_failed_upload(form_num: int, failed: bool) -> None:
         global failed_data_upload
         failed_data_upload = failed
 
+# Used in navigations, gets rid of any error messages from failed uploads/downloads
 def reset_failures():
     global failed_video_upload
     failed_video_upload = False
@@ -98,12 +105,14 @@ def get_video_list() -> list:
 def get_data_file_list() -> list:
     return data_file_list
 
+# Used for the name of the folder holding video/data files when downloaded from a link
 def get_folder_name(form_num: int) -> str:
     if form_num == 1:
         return video_folder_name
     elif form_num == 2:
         return data_folder_name
 
+# Removes all files within a list passed in, does error checking first
 def delete_files_in_list(listed_files) -> None:
     for file in listed_files:
         if os.path.isfile(file):
@@ -111,6 +120,7 @@ def delete_files_in_list(listed_files) -> None:
 
 #Automatically deletes populated files on exit of python program
 #could add a feature where users choose to keep these files(??)
+#If a link download was accomplished, deletes the folder created from that action
 def delete_files_on_exit() -> None:
     for file in video_file_list:
         if os.path.isfile(file):
@@ -127,6 +137,7 @@ def delete_files_on_exit() -> None:
         if os.path.exists(flask_folder):
             shutil.rmtree(flask_folder)
 
+#This function validates that the link submitted is an actual link, and goes to the correct website w/ downloadable (can't really go further)
 def validate_link(link: str, flag: int) -> bool:
     if "." not in link:
         return False
@@ -139,6 +150,7 @@ def validate_link(link: str, flag: int) -> bool:
 
 # The original code for this function was given to us by Brian Szekely, a PhD student and former student of
 # Dr. MacNeilage's Self-Motion Lab. It has been slightly altered to fit our code.
+# This function takes in a link, downloads a zip file from that destination, and unzips it
 def extract_unzip(link: str) -> bool:
     zip_url = link
     response = requests.get(zip_url)
@@ -151,6 +163,7 @@ def extract_unzip(link: str) -> bool:
     zip_file.extractall(filepath)
     return True
 
+#This function validates that the files uploaded for videos are correct, some name checks some file ext. checks
 def validate_video_files(file_list) -> bool:
     file_count = len(file_list)
 
@@ -174,6 +187,7 @@ def validate_video_files(file_list) -> bool:
     else:
         return False
 
+# This function validates that the files uploaded for data are correct, can do this from naming convention
 def validate_data_files(file_list) -> bool:
     file_count = len(file_list)
     if file_count > 15 or file_count < 10:
@@ -191,7 +205,7 @@ def validate_data_files(file_list) -> bool:
 
 atexit.register(delete_files_on_exit)
 
-# @app.route('/')
+# Initial screen, with both files ready for upload
 def main():
     global show_form1
     show_form1 = True
@@ -201,7 +215,7 @@ def main():
 
     return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2)
 
-# @app.route('/upload_video', methods=['POST'])
+# Ran when upload video files button is clicked, checks and validates files that were uploaded
 def upload_video():
     if request.method == 'POST':
         reset_failures()
@@ -252,7 +266,7 @@ def upload_video():
         set_showform(1, False)
         return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2)
 
-# @app.route('/upload_data', methods=['POST'])
+# Ran when upload data files button is clicked, checks and validates files that were uploaded
 def upload_data():
     if request.method == 'POST':
         reset_failures()
@@ -283,7 +297,7 @@ def upload_data():
         set_showform(2, False)
         return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2)
 
-# @app.route('/upload_video_link', methods=['POST'])
+# This function is ran when the upload video link form is submitted, attempts to download and validate the files found at that link
 def upload_video_link():
     if request.method == 'POST':
         reset_failures()
@@ -311,50 +325,56 @@ def upload_video_link():
         global video_folder_name
         for folder in folders_list:
             #Should pick out unzipped folder containing videos
+            folder_name = folder.name
             if "-" in folder and "pycache" not in folder:
-                video_folder_name = folder
+                video_folder_name = folder_name
                 break
 
-        video_dir = current_working_dir + video_folder_name
-        global data_file_list
+        video_dir = current_working_dir + '\\' + "flaskr" + '\\' + video_folder_name + '\\'
+        global video_file_list
         for file in video_dir:
             video_file_list.append(file)
 
-        if validate_data_files(video_file_list) is False:
+        if validate_video_files(video_file_list) is False:
             delete_files_in_list(video_file_list)
-            #return something to report that link downloaded wrong files
+            set_failed_upload(1, True)
+            return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2,
+                                   failed_data_link=failed_data_link, failed_video_link=failed_video_link)
 
         # Runs if files are correctly uploaded and validated
         set_showform(2, False)
-        set_is_folder(2, True)
+        set_is_folder(2, True, video_folder_name)
 
+        new_video_file_list = []
+        for file in video_file_list:
+            complete_file_path = "flaskr" + "\\" + video_folder_name + "\\" + file
+            new_video_file_list.append(complete_file_path)
+        video_file_list.clear()
+        video_file_list = new_video_file_list
+
+        # all of this code is to ensure files are found in the static folder, which is how the HTML runs them
         for filename in video_file_list:
             if "worldPrivate" in filename:
-                os.rename(filename, "static/worldvideo.mp4")
+                os.rename(filename, "flaskr/static/worldvideo.mp4")
             elif "eye0" in filename:
-                os.rename(filename, "static/eye0.mp4")
+                os.rename(filename, "flaskr/static/eye0.mp4")
             elif "eye1" in filename:
-                os.rename(filename, "static/eye1.mp4")
+                os.rename(filename, "flaskr/static/eye1.mp4")
             elif "csv" in filename:
-                os.rename(filename, "static/datatable.csv")
+                os.rename(filename, "flaskr/static/datatable.csv")
 
         video_file_list.clear()
-        video_file_list.append("static/worldvideo.mp4")
-        video_file_list.append("static/eye0.mp4")
-        video_file_list.append("static/eye1.mp4")
-        video_file_list.append("static/datatable.csv")
-
-        for file in video_file_list:
-            complete_file_path = video_folder_name + "/" + file
-            video_file_list.append(complete_file_path)
-            video_file_list.remove(file)
+        video_file_list.append("flaskr/static/worldvideo.mp4")
+        video_file_list.append("flaskr/static/eye0.mp4")
+        video_file_list.append("flaskr/static/eye1.mp4")
+        video_file_list.append("flaskr/static/datatable.csv")
 
         if get_showform(1) == False and get_showform(2) == False:
             return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2)
         else:
             return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2)
 
-# @app.route('/upload_data_link', methods=['POST'])
+# This function is ran when the upload data link form is submitted, attempts to download and validate the files found at that link
 def upload_data_link():
     if request.method == 'POST':
         reset_failures()
@@ -379,27 +399,24 @@ def upload_data_link():
             if folder.is_dir():
                 folders_list.append(folder)
 
-        print(folders_list)
         global data_folder_name
         for folder in folders_list:
             folder_name = folder.name
             if "fixation" not in folder_name and "static" not in folder_name and "templates" not in folder_name and "__pycache__" not in folder_name:
                 if "-" not in folder_name and "MAC" not in folder_name:
                     data_folder_name = folder_name
-                    print(data_folder_name)
                     break
 
         data_dir = current_working_dir + '\\' + "flaskr" + '\\' + data_folder_name + '\\'
-        print(data_dir)
         files = os.listdir(data_dir)
         global data_file_list
         for file in files:
             data_file_list.append(file)
 
-        print(data_file_list)
 
         if validate_data_files(data_file_list) is False:
             delete_files_in_list(data_file_list)
+            set_failed_upload(2, True)
             return render_template("file-upload/file_upload.html", show_form1=show_form1, show_form2=show_form2,
                                    failed_data_link=failed_data_link, failed_video_link=failed_video_link)
 
