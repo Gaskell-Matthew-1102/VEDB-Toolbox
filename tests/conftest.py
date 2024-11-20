bfd87ac249890297ba008f0d3f3e95bf5805d640
Gaskell-Matthew-1102 <112650395+Gaskell-Matthew-1102@users.noreply.github.com>
2024-11-20 09:09:55 -0800

    Additions to and refactoring of testing code, still needs work in some areas

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,36 @@
+# When writing a lot of this testing code I referenced this resource for some assistance: https://testdriven.io/blog/flask-pytest/
+
 import os
 import pytest
+from werkzeug.security import generate_password_hash
 
-import flaskr.file_upload
-from flaskr.file_upload import app
+from flaskr.models import Users
+from flaskr import create_app, db
 
+@pytest.fixture(scope='module')
+def new_test_user():
+    user = Users(username='example', email='user@email.com', password=generate_password_hash('password'), administrator=True)
+    return user
+
+@pytest.fixture(scope='module')
+def test_client():
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        with flask_app.app_context():
+            yield test_client
+
+@pytest.fixture(scope='module')
+def init_database(test_client, new_test_user):
+    db.create_all()
+    default_user = new_test_user
+    db.session.add(default_user)
+    db.session.commit()
+    yield
+    db.drop_all()
+
+@pytest.fixture(scope='module')
+def test_login(test_client):
+    test_client.post('/login', data={'username': 'testlogin', 'password': 'testpassword'})
+    yield
+    test_client.get('/logout')
--- tests/functional/test_upload_file.py
+++ tests/functional/test_upload_file.py
@@ -1,60 +1,68 @@
-import pytest
 import os
+from flaskr.file_upload import get_showform
 from flaskr import create_app
-import flaskr.file_upload
-from flaskr.file_upload import app, validate_link, validate_video_files, validate_data_files, get_showform
 
-
-# from flaskr.visualizer import data_list
-
-
+#this currently fails because there is a check for user authenticated
 def test_file_upload_initial_route():
     #This will probably be used when we link together all the files
-    #os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
-    #app = create_app('testing')
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert b"Upload Files" in response.data
-    #Form to upload video files
-    assert b'<form action = "/upload_video" method="POST" enctype="multipart/form-data">' in response.data
-    #Form to upload data files
-    assert b'<form action = "/upload_data" method="POST" enctype="multipart/form-data">' in response.data
-    #Form to paste link for download of video files
-    assert b'<form action = "/upload_video_link" method="POST" enctype="multipart/form-data">' in response.data
-    #Form to paste link for download of data files
-    assert b'<form action = "/upload_data_link" method="POST" enctype="multipart/form-data">' in response.data
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    app = create_app()
+    with app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+        assert b"Upload Files" in response.data
+        #Form to upload video files
+        assert b'<form action = "/upload_video" method="POST" enctype="multipart/form-data">' in response.data
+        #Form to upload data files
+        assert b'<form action = "/upload_data" method="POST" enctype="multipart/form-data">' in response.data
+        #Form to paste link for download of video files
+        assert b'<form action = "/upload_video_link" method="POST" enctype="multipart/form-data">' in response.data
+        #Form to paste link for download of data files
+        assert b'<form action = "/upload_data_link" method="POST" enctype="multipart/form-data">' in response.data
 
 def test_file_help_route():
-    response = app.test_client().get('/upload_help')
-    assert response.status_code == 200
-    #Title
-    assert b"How To Upload Files" in response.data
-    #Infoblock 1 is the help section for video files
-    assert b"infoblock1" in response.data
-    #Infoblock 2 is the help section for data files
-    assert b"infoblock2" in response.data
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/upload_help')
+        assert response.status_code == 200
+        #Title
+        assert b"How To Upload Files" in response.data
+        #Infoblock 1 is the help section for video files
+        assert b"infoblock1" in response.data
+        #Infoblock 2 is the help section for data files
+        assert b"infoblock2" in response.data
 
 def test_back_to_upload():
-    response = app.test_client().post('/go_back')
-    assert response.status_code == 200
-    assert b"Upload Files" in response.data
-    assert b'<form action = "/upload_video" method="POST" enctype="multipart/form-data">' in response.data
-    assert b'<form action = "/upload_data" method="POST" enctype="multipart/form-data">' in response.data
-    assert b'<form action = "/upload_video_link" method="POST" enctype="multipart/form-data">' in response.data
-    assert b'<form action = "/upload_data_link" method="POST" enctype="multipart/form-data">' in response.data
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/go_back')
+        assert response.status_code == 200
+        assert b"Upload Files" in response.data
+        assert b'<form action = "/upload_video" method="POST" enctype="multipart/form-data">' in response.data
+        assert b'<form action = "/upload_data" method="POST" enctype="multipart/form-data">' in response.data
+        assert b'<form action = "/upload_video_link" method="POST" enctype="multipart/form-data">' in response.data
+        assert b'<form action = "/upload_data_link" method="POST" enctype="multipart/form-data">' in response.data
 
 def test_upload_different_video():
-    response = app.test_client().get('/upload_different_video')
-    assert response.status_code == 405
-    response = app.test_client().post('/upload_different_video')
-    assert response.status_code == 200
-    assert get_showform(1) is True
-    assert b'<form action = "/upload_video" method="POST" enctype="multipart/form-data">' in response.data
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/upload_different_video')
+        assert response.status_code == 405
+        response = test_client.post('/upload_different_video')
+        assert response.status_code == 200
+        assert get_showform(1) is True
+        assert b'<form action = "/upload_video" method="POST" enctype="multipart/form-data">' in response.data
 
 def test_upload_different_data():
-    response = app.test_client().get('/upload_different_data')
-    assert response.status_code == 405
-    response = app.test_client().post('/upload_different_data')
-    assert response.status_code == 200
-    assert get_showform(2) is True
-    assert b'<form action = "/upload_data" method="POST" enctype="multipart/form-data">' in response.data
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/upload_different_data')
+        assert response.status_code == 405
+        response = test_client.post('/upload_different_data')
+        assert response.status_code == 200
+        assert get_showform(2) is True
+        assert b'<form action = "/upload_data" method="POST" enctype="multipart/form-data">' in response.data
