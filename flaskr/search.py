fdf58201127080e30f5118815b92ac9386f17abf
MagbanLeon <lmagbanua@unr.edu>
2024-11-30 17:33:48 -0800

    eamil can now be used in search bar as well

--- flaskr/search.py
+++ flaskr/search.py
@@ -9,6 +9,8 @@ from .forms import LoginForm, RegistrationForm
 def searchBar(search_filter):
     if search_filter == "none":
         list = Users.query.with_entities(Users.username, Users.email, Users.administrator).all()
+    elif '@' in search_filter:
+        list = Users.query.with_entities(Users.username, Users.email, Users.administrator).filter_by(email=search_filter).all()
     else:
         list = Users.query.with_entities(Users.username, Users.email, Users.administrator).filter_by(username=search_filter).all()
     return list
