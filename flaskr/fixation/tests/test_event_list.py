351a1b973efd3b4d5099b1c25352640b19cd76d7
Tyler <tdishman@nevada.unr.edu>
2025-03-20 18:21:20 -0700

    Implemented short fixation filter tests

--- flaskr/fixation/tests/test_event_list.py
+++ flaskr/fixation/tests/test_event_list.py
@@ -98,5 +98,88 @@ class TestEventList:
 
         assert obj == test
 
+    def test_short_fixation_filter(self):
+        X = 0
+        Y = 1
+        event1 = Event(Event.Sample_Type.GAP, 0, 1, [X, X], [X, Y])
+        event2 = Event(Event.Sample_Type.FIXATION, 1, 1.009, [0, 0], [1, 1])
+        event3 = Event(Event.Sample_Type.GAP, 1.009, 2.5, [Y, X], [Y, Y])
+        obj = EventList(np.array([event1, event2, event3]))
+        obj.apply_filter(Event.short_fixation_filter, min_fixation_dur_ms=70)
+
+        ex_event1 = Event(Event.Sample_Type.GAP, 0, 2.5, [X, X], [Y, Y])
+        test_list = EventList(np.array([ex_event1]))
+
+        assert obj == test_list
+
+    def test_eventList_short_fixation_filter_beginning(self):
+        X = 0
+        Y = 1
+        event2 = Event(Event.Sample_Type.FIXATION, 1, 1.009, [0, 0], [100, 1])
+        event3 = Event(Event.Sample_Type.GAP, 1.009, 2.5, [Y, X], [Y, Y])
+        obj = EventList(np.array([event2, event3]))
+        obj.apply_filter(Event.short_fixation_filter, min_fixation_dur_ms=70)
+
+        ex_event1 = Event(Event.Sample_Type.GAP, 1.009, 2.5, [Y, X], [Y, Y])
+        test_list = EventList(np.array([ex_event1]))
+
+        assert obj == test_list
+
+    def test_eventList_short_fixation_filter_end(self):
+        X = 0
+        Y = 1
+        event1 = Event(Event.Sample_Type.GAP, 0, 1, [X, X], [X, Y])
+        event2 = Event(Event.Sample_Type.FIXATION, 1, 1.009, [0, 0], [100, 1])
+        obj = EventList(np.array([event1, event2]))
+        obj.apply_filter(Event.short_fixation_filter, min_fixation_dur_ms=70)
+
+        ex_event1 = Event(Event.Sample_Type.GAP, 0, 1, [X, X], [X, Y])
+        test_list = EventList(np.array([ex_event1]))
+
+        assert obj == test_list
+
+    def test_eventList_short_fixation_filter_large(self):
+        X = 0
+        Y = 1
+        event1 = Event(Event.Sample_Type.FIXATION, 0, 0.009, [0, 0], [100, 1])
+        event2 = Event(Event.Sample_Type.GAP, 0.009, 1, [X, X], [X, Y])
+        event3 = Event(Event.Sample_Type.FIXATION, 1, 1.009, [0, 0], [100, 1])
+        event4 = Event(Event.Sample_Type.GAP, 1.009, 1.5, [Y, X], [Y, Y])
+        event5 = Event(Event.Sample_Type.FIXATION, 1.5, 1.8, [0, 0], [1, 1])
+        event6 = Event(Event.Sample_Type.GAP, 1.5005, 2, [X, X], [X, Y])
+        event7 = Event(Event.Sample_Type.FIXATION, 2, 2.009, [0, 0], [100, 1])
+        obj = EventList(np.array([event1, event2, event3, event4, event5, event6, event7]))
+
+
+        ex_ev1 = Event(Event.Sample_Type.GAP, 0.009, 1.5, [X, X], [Y, Y])
+        ex_ev2 = Event(Event.Sample_Type.FIXATION, 1.5, 1.8, [0, 0], [1, 1])
+        ex_ev3 = Event(Event.Sample_Type.GAP, 1.5005, 2, [X, X], [X, Y])
+        test = EventList(np.array([ex_ev1, ex_ev2, ex_ev3]))
+
+        obj.apply_filter(Event.short_fixation_filter, min_fixation_dur_ms=70)
+
+        assert obj == test
+
+    def test_eventList_all_filters(self):
+        X = 0
+        Y = 1
+        event1 = Event(Event.Sample_Type.FIXATION, 0, 0.009, [0, 0], [100, 1])
+        event2 = Event(Event.Sample_Type.GAP, 0.009, 1, [X, X], [X, Y])
+        event3 = Event(Event.Sample_Type.FIXATION, 1, 1.009, [0, 0], [100, 1])
+        event4 = Event(Event.Sample_Type.GAP, 1.009, 1.5, [Y, X], [Y, Y])
+        event5 = Event(Event.Sample_Type.FIXATION, 1.5, 1.55, [0, 0], [1, 1])
+        event6 = Event(Event.Sample_Type.GAP, 1.5005, 2, [X, X], [X, Y])
+        event7 = Event(Event.Sample_Type.FIXATION, 2, 2.009, [0, 0], [100, 1])
+        obj = EventList(np.array([event1, event2, event3, event4, event5, event6, event7]))
+
+
+        ex_ev1 = Event(Event.Sample_Type.GAP, 0.009, 2, [X, X], [X, Y])
+        test = EventList(np.array([ex_ev1]))
+
+        obj.apply_filter(Event.microsaccade_filter, min_saccade_amp_deg=1.0, min_saccade_dur_ms=10)
+        obj.apply_filter(Event.short_fixation_filter, min_fixation_dur_ms=70)
+
+        assert obj == test
+
     # def test_eventList_iter(setup):
     #     for event in setup.obj:
